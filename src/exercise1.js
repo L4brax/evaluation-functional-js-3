765e48860d1e8df289779de64993f09c6a53f289
L4brax <ant.hess2@gmail.com>
2018-03-02 10:13:07 +0100

    Exercise 2 + Start of exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,7 +1,10 @@
 const moment = require("moment");
 
 const sortProduct = input => {
-  // Your future job begins here ...
+  return {
+    tomato : input
+      .filter(value => moment(value.dateAdded).toDate() > moment().year(2000).toDate())
+  }
 };
 
 module.exports = {
--- test/exercise2.test.js
+++ test/exercise2.test.js
@@ -1,11 +1,38 @@
 const expect = require('chai').expect;
 const program = require('../src/exercise2');
 
-describe(`${program.title}`, function () {
+describe.only(`${program.title}`, function () {
 
   it('Function yolo with mysterious parameter should return a list of messages', function () {
 
-    const whereIsMyMind = "???";
+    const whereIsMyMind = [{
+      name : 'Number six',
+      sex : 9
+    },
+    {
+      name : 'Number height',
+      sex : 9
+    },
+    {
+      name : 'William',
+      sex : 1
+    },
+    {
+      name : 'Gaius',
+      sex : 1
+    },
+    {
+      name : 'Lee',
+      sex : 1
+    },
+    {
+      name : 'Kara',
+      sex : 2
+    },
+    {
+      name : 'Laura',
+      sex : 2
+    }];
 
     const output = [
       "0 not known character(s) ",
